a142c8797942f870a78469703e14d3ba5b36d277
kadrianne <kristine.a.du@gmail.com>
2021-03-02 23:36:52 -0700

    Add countdown, details, and styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Countdown, { zeroPad } from 'react-countdown';
 
 function App() {
+  const partyDate = 1617973200000;
+
+  const Complete = () => <h2>🎉</h2>
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <Complete />;
+    } else {
+      return (
+        <div className='countdown'>
+          <p><span className='time'>{zeroPad(days)}</span> DAYS</p>
+          <p><span className='time'>{zeroPad(hours)}</span> HOURS</p>
+          <p><span className='time'>{zeroPad(minutes)}</span> MINUTES</p>
+          <p><span className='time'>{zeroPad(seconds)}</span> SECONDS</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>MAE'S LAST FLING<br/>BEFORE THE NG</h1>
+        <section className='details'>
+          <p>april 9-11, 2021</p>
+          <p>red hook, ny</p>
+        </section>
+        <Countdown date={partyDate} renderer={renderer} />
+        <h2>schedule coming soon!</h2>
+      </main>
     </div>
   );
 }
